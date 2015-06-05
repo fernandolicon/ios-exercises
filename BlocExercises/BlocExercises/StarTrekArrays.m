79e4bc00e3be92254065ed05670a320f11aa7ff4
fernandolicon <fernando.mata.licon@gmail.com>
2015-06-05 17:28:53 -0600

    All StarTrekArrays tests succeeded.

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,22 +11,56 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *starTrekCharacters = [[NSMutableArray alloc] init];
+    
+    unsigned long len = [characterString length];
+    const char *buffer = [characterString UTF8String];
+    NSMutableString *name = [[NSMutableString alloc] initWithString:@""];
+    
+    for(int i = 0; i < len; ++i) {
+        char current = buffer[i];
+        if (current != ';') {
+            [name appendFormat:@"%c", current];
+        }else{
+            NSString *stringName = [NSString stringWithString:name];
+            [starTrekCharacters addObject:stringName];
+            [name setString:@""];
+        }
+    }
+    [starTrekCharacters addObject:name];
+    
+    return starTrekCharacters;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    NSMutableString *names = [[NSMutableString alloc] init];
+    int count = 0;
+    for (NSString *name in characterArray){
+        if (count == 0) {
+            [names appendString:name];
+        }else{
+            [names appendFormat:@";%@", name];
+        }
+        count++;
+    }
+    
+    NSString *finalNames = [NSString stringWithString:names];
+    return finalNames;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    NSArray *sortedArray = [characterArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    return sortedArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
+    for (NSString *text in characterArray) {
+        NSString *lowerCaseText = [text lowercaseString];
+        if ([lowerCaseText containsString:@"worf"]) {
+            return YES;
+        }
+    }
+    
     return NO;
 }
 
